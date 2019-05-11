5147df3589f816526d0306b481d95341b262e1d8
DhanajeyanN <DhanajeyanN@Dhanajeyan.aosta.com>
2019-05-11 13:10:19 +0530

    First Commit

---
+++ Employee/src/main/java/Aosta/Employee/Employee.java
@@ -0,0 +1,39 @@
+package Aosta.Employee;
+
+public class Employee {
+
+	private String id;
+	private String name;
+	private String department;
+
+	
+	protected Employee(String id, String name, String department) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.department = department;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	
+	
+	
+	
+}
